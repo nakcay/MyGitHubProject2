f22964e52ca6f20166641fcd13f6fdcc347249ee
nurdane <nurdane81@gmail.com>
2023-02-01 15:40:06 -0700

    some changes on done second class

--- src/day2GitHub/SecondGitHubClass.java
+++ src/day2GitHub/SecondGitHubClass.java
@@ -2,6 +2,8 @@ package day2GitHub;
 
 public class SecondGitHubClass {
     public static void main(String[] args) {
+
         System.out.println(" I am learning GitHub");
+        System.out.println(2+2);
     }
 }
